6d92ea5a320d62a6ee02cb168b49b6e1c1724fcc
aawezk456 <aawezk456@gmail.com>
2020-08-15 20:17:56 +0530

    Added Model user

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const Mongoose = require('mongoose');
+
+const { Schema } = Mongoose;
+
+// User Schema
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: () => {
+      return this.provider !== 'email' ? false : true;
+    }
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  provider: {
+    type: String,
+    required: true,
+    default: 'email'
+  },
+  googleId: {
+    type: String,
+    unique: true
+  },
+  facebookId: {
+    type: String,
+    unique: true
+  },
+  avatar: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: ['ROLE_MEMBER', 'ROLE_ADMIN', 'ROLE_MERCHANT'],
+    default: 'ROLE_MEMBER'
+  },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Mongoose.model('User', UserSchema);
